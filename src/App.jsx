2aed5fb3381c57cc4a2927a5d4d27aa5176fca8e
Carmeshia Lazzana <65563803+Meshia13@users.noreply.github.com>
2024-11-02 11:46:45 -0400

    Created first function component todo list in JSX file

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+
+// Above the App() function, create an empty Array and store it in a variable named todoList
+// Inside the Array, create at least 3 Objects with the following properties: id: unique identifier, title: summary of the todo item 
+const todoList = [
+
+  { id: 1, title: "Go to Carribean"},
+  { id: 2, title: "Complete a 5k Race"},
+  { id: 3, title: "Complete 30-day workout challenge"}
+
+];
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    {/* Create a level-one heading that says "Todo List" */}
+    <h1>Todo List</h1>
+
+    {/*Create an unordered list, Inside your unordered list, insert a JavaScript expression hint: {} */}
+    <ul id="myList">
+
+      {/*Inside the JavaScript expression, map over your todoList array */}
+      {todoList.map(function (item) {
+       
+       {/* For each Object in Array, return a list item (<li>) with the following: key attribute with value of id property,
+        inner text content with value of title property*/}
+       return (
+          <li key={item.id} >
+            {item.title}
+          </li>
+        )
+
+      })}
+
+    </ul>
+
+
     </>
+
   )
+
 }
 
 export default App
