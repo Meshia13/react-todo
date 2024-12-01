9c2e28a6f7fa343ae9a3fd24ee2c9fc511a2057c
Carmeshia Lazzana <65563803+Meshia13@users.noreply.github.com>
2024-12-01 18:45:45 -0500

    Add Auto-Focus to InputWithLabel.jsx

--- src/InputWithLabel.jsx
+++ src/InputWithLabel.jsx
@@ -1,6 +1,17 @@
+import { useEffect, useRef } from "react"
 
 // Add props as a parameter in the InputWithLabel function update todoTitle and handleTitleChange references to come from props
 export default function InputWithLabel(props) {
+
+    // Use the useRef React hook to create an imperative ref named inputRef
+    const inputRef = useRef();
+
+    // Define a useEffect React hook without dependency list
+    useEffect(() => {
+        //  Inside the side-effect handler function, call the focus() method on the current inputRef
+        inputRef.current.focus();
+    })
+
     return (
         <>
         {/* Replace the text inside the label element with a new props variable named label 
@@ -11,7 +22,9 @@ export default function InputWithLabel(props) {
         id="todoTitle" 
         name="title" 
         value={props.todoTitle} 
-        onChange={props.handleTitleChange} />
+        onChange={props.handleTitleChange}
+        // Add a ref prop with value inputRef on the input element
+        ref={inputRef} />
 
         </>
     )
