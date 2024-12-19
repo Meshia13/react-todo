ab29ede35c75b32037426e8108b24247257a335e
Carmeshia Lazzana <65563803+Meshia13@users.noreply.github.com>
2024-12-19 17:31:36 -0500

    added notes to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,25 @@ import {useState, useEffect} from "react";
 
 function App() {
 
-  // const [todoList, setTodoList] = useState(JSON.parse(localStorage.getItem("savedTodoList")) || [todoList]);
   const [todoList, setTodoList] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
-  /*  */
+  /* define a useEffect React hook with an empty dependency list
+  Inside the side-effect handler function, define a new Promise and pass in a callback function with parameters resolve and reject
+  inside the callback function declare a timeout; callback: function with no parameters delay time: 2000 milliseconds (2 seconds)  */
   useEffect(() => {
     new Promise((resolve, reject) => {
       setTimeout(
         () => {
-          const savedTodoList = JSON.parse(localStorage.getItem("savedTodoList")) || [todoList];
+          /* call the parameter resolve which is a callback function for when the Promise is successful and pass it an as Object with property data as a nested empty Object
+          Inside the data object, add a property todoList and set it's value to the initial/default list state (copy from useState hook)
+           Update the default state for todoList to be an empty Array */
+          const savedTodoList = JSON.parse(localStorage.getItem("savedTodoList")) || [];
           resolve({data : {todoList: savedTodoList}})
         }, 2000)
     })
+    /* Chain a then method to your Promise constructor and pass it a function with parameter result
+    Inside the function, use your state setter to update the list and pass the todoList from your result object */
     .then(result => {
       setTodoList(result.data.todoList);
       setIsLoading(false);
@@ -28,6 +34,7 @@ function App() {
   // Inside the side-effect handler function, save the todoList inside localStorage with the key "savedTodoList"
   useEffect(() => {
 
+    // Inside the second useEffect hook (with todoList dependency), add an if statement to check that isLoading is false before setting localStorage
     if (!isLoading) {
       localStorage.setItem("savedTodoList", JSON.stringify(todoList));
     }
@@ -63,11 +70,11 @@ function App() {
       {/* Create a level-one heading that says "Todo List" */}
       <h1>Todo List</h1>
 
-      {/* Below the level-one heading, use the AddTodoForm component */}
       {/* Pass setNewTodo as a callback handler prop named onAddTodo to the AddTodoForm component 
       -Change the value of the onAddTodo prop for AddTodoForm to addTodo*/}
       <AddTodoForm onAddTodo={addTodo} />
       
+      {/*  Using a ternary operator inside JSX, if isLoading is true render the loading message, otherwise render the TodoList component */}
       {isLoading ? (
         <p>Loading...</p>
       ) : (
