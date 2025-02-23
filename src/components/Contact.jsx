eb8cbb6eeb0cd02ded3a70401cd333ba4811e263
Carmeshia Lazzana <65563803+Meshia13@users.noreply.github.com>
2025-02-23 16:03:24 -0500

    fixed media queries and changed fonts

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,7 +7,12 @@ function Contact() {
             <NavBar />
 
             <div >
-            <h1>Contact Us!</h1>
+            <h1 style={{fontFamily: "Flavors",
+                fontSize: '5rem',
+                fontWeight: '200',
+                fontStyle: 'normal',
+                color: 'rgb(113, 8, 154)'
+            }}>Contact Us!</h1>
             
             </div>
            
--- src/components/Subscribe.jsx
+++ src/components/Subscribe.jsx
@@ -7,7 +7,12 @@ function Subscribe() {
             <NavBar />
 
             <div >
-            <h1>Subscribe!</h1>
+            <h1 style={{fontFamily: "Flavors",
+                fontSize: '5rem',
+                fontWeight: '200',
+                fontStyle: 'normal',
+                color: 'rgb(113, 8, 154)'
+            }}>Subscribe!</h1>
             
             </div>
            
